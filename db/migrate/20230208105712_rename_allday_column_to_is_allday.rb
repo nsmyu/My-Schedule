b2b29b19683441525ca3e37ea1f4a0ab0cbabdec
西村優子 <yuko@nishimurayuukonoMacBook-Pro.local>
2023-02-15 18:39:34 +0900

    エラー解決のため不要なマイグレーションファイルを削除

--- db/migrate/20230208105712_rename_allday_column_to_is_allday.rb
+++
@@ -1,5 +0,0 @@
-class RenameAlldayColumnToIsAllday < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :schedules, :allday, :is_allday
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_08_105712) do
+ActiveRecord::Schema.define(version: 2023_02_06_123946) do
 
   create_table "schedules", force: :cascade do |t|
     t.string "title"
